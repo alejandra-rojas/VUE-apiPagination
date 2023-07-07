f111fb7a9c13a8a10e70b1cadeb4b5417d2a01c6
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-07-07 16:54:34 +0200

    Pagination component

---
+++ src/views/Employees/Pagination.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+    <div class="pagination-container">
+      <div
+        v-for="pageNumber in totalPages"
+        :key="pageNumber"
+        @click="changePage(pageNumber)"
+        :class="{
+          pagination: true,
+          activePagination: this.currentPage === pageNumber,
+        }"
+        class="pagination"
+      >
+        {{ pageNumber }}
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["totalPages", "currentPage"],
+
+  methods: {
+    changePage(pageNumber) {
+      this.$emit("pagechanged", pageNumber);
+    },
+  },
+};
+</script>
+
+<style>
+.pagination-container {
+  margin-top: 40px;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+
+.pagination {
+  cursor: pointer;
+}
+</style>
