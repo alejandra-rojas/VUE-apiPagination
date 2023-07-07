62103c8c3a39792119c4cc7eb3a52dde2b342210
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-07-07 17:00:39 +0200

    Content change - about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="about">
+    <h2>About this coding exercise</h2>
+    <p>
+      The objective of this project was to output data from an API.
+    </p>
+    <p>
+      As two API endpoints were provided, pagination was implemented. 
+    </p>
+    <p>Requirements from the design team:
+      <ul><li>Round profile pictures</li><li>Name, followed by a contact link that leads to
+      the employees email</li><li>  Responsive design</li></ul>
+    </p>
+    <p>Vue was the required framework for the excercise.</p>
+  </section>
 </template>
+
+<style >
+.about li {
+  list-style: none;
+}
+
+.about p {
+  font-size: 1.2rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <h2>Hi Sara!</h2>
     <p>Thank you for taking the time to review my exercise.</p>
     <p>In the employees tab, you can demo my solution,</p>
-    <p>kind regards. Alejandra</p>
+    <p>kind regards - Alejandra</p>
   </section>
 </template>
 
